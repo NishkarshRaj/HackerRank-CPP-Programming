6c500f077a40c87b899c776f34ffbda485f2aa12
Nishkarsh Raj <500060720@stu.upes.ac.in>
2019-06-19 13:18:59 +0530

    Data Type code is added

---
+++ DataType.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a;
+	long b;
+	char c;
+	float d;
+	double e;
+	cin>>a>>b>>c>>d>>e;
+	cout<<a<<endl<<b<<endl<<c<<endl;
+	printf("%0.3f",d);
+	printf("\n");
+	printf("%0.9e",e);
+}
